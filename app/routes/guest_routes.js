d1ac5e15e5590a760dfa6984a6189b78688ff39a
mjeder <matteder96@gmail.com>
2021-04-14 12:34:47 -0500

    Move waitlist ID to params instead of body in DESTROY

--- app/routes/guest_routes.js
+++ app/routes/guest_routes.js
@@ -6,32 +6,32 @@ const Waitlist = require('../models/waitlist')
 const customErrors = require('../../lib/custom_errors')
 
 const handle404 = customErrors.handle404
-const requireOwnership = customErrors.requireOwnership
+// const requireOwnership = customErrors.requireOwnership
 
 const removeBlanks = require('../../lib/remove_blank_fields')
 const requireToken = passport.authenticate('bearer', { session: false })
 
 const router = express.Router()
 
-// INDEX
-// GET /guests
-router.get('/guests', requireToken, (req, res, next) => {
-  Waitlist.find()
-    .then(guests => {
-      return guests.map(guest => guest.toObject())
-    })
-    .then(guests => res.status(200).json({ guests: guests }))
-    .catch(next)
-})
-
-// SHOW
-// GET /guests/5a7db6c74d55bc51bdf39793
-router.get('/guests/:id', requireToken, (req, res, next) => {
-  Waitlist.findById(req.params.id)
-    .then(handle404)
-    .then(guest => res.status(200).json({ guest: guest.toObject() }))
-    .catch(next)
-})
+// // INDEX
+// // GET /guests
+// router.get('/guests', requireToken, (req, res, next) => {
+//   Waitlist.find()
+//     .then(guests => {
+//       return guests.map(guest => guest.toObject())
+//     })
+//     .then(guests => res.status(200).json({ guests: guests }))
+//     .catch(next)
+// })
+//
+// // SHOW
+// // GET /guests/5a7db6c74d55bc51bdf39793
+// router.get('/guests/:id', requireToken, (req, res, next) => {
+//   Waitlist.findById(req.params.id)
+//     .then(handle404)
+//     .then(guest => res.status(200).json({ guest: guest.toObject() }))
+//     .catch(next)
+// })
 
 // CREATE
 // POST /guests
@@ -51,7 +51,7 @@ router.post('/guests', requireToken, (req, res, next) => {
 
 // UPDATE
 // PATCH /guests/5a7db6c74d55bc51bdf39793
-router.patch('/guests/:id', requireToken, removeBlanks, (req, res, next) => {
+router.patch('/guests/:id', (req, res, next) => {
   // delete req.body.guest.owner
   const guestId = req.params.id
   const data = req.body.guest
@@ -69,12 +69,15 @@ router.patch('/guests/:id', requireToken, removeBlanks, (req, res, next) => {
 
 // DESTROY
 // DELETE /guests/5a7db6c74d55bc51bdf39793
-router.delete('/guests/:id', requireToken, (req, res, next) => {
-  Waitlist.findById(req.params.id)
+router.delete('/guests/:waitlistId/:guestId', (req, res, next) => {
+  const guestId = req.params.guestId
+  const waitlistId = req.params.waitlistId
+  Waitlist.findById(waitlistId)
     .then(handle404)
-    .then(guest => {
-      requireOwnership(req, guest)
-      guest.deleteOne()
+    .then(waitlist => {
+      waitlist.guests.id(guestId).remove()
+
+      return waitlist.save()
     })
     .then(() => res.sendStatus(204))
     .catch(next)
