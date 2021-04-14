706ae961aba687a9fb923b1f7ae23f0ec4cf2bf7
mjeder <matteder96@gmail.com>
2021-04-14 13:26:10 -0500

    Move waitlistId to params in Update/Create

--- app/routes/guest_routes.js
+++ app/routes/guest_routes.js
@@ -8,37 +8,16 @@ const customErrors = require('../../lib/custom_errors')
 const handle404 = customErrors.handle404
 // const requireOwnership = customErrors.requireOwnership
 
-const removeBlanks = require('../../lib/remove_blank_fields')
+// const removeBlanks = require('../../lib/remove_blank_fields')
 const requireToken = passport.authenticate('bearer', { session: false })
 
 const router = express.Router()
 
-// // INDEX
-// // GET /guests
-// router.get('/guests', requireToken, (req, res, next) => {
-//   Waitlist.find()
-//     .then(guests => {
-//       return guests.map(guest => guest.toObject())
-//     })
-//     .then(guests => res.status(200).json({ guests: guests }))
-//     .catch(next)
-// })
-//
-// // SHOW
-// // GET /guests/5a7db6c74d55bc51bdf39793
-// router.get('/guests/:id', requireToken, (req, res, next) => {
-//   Waitlist.findById(req.params.id)
-//     .then(handle404)
-//     .then(guest => res.status(200).json({ guest: guest.toObject() }))
-//     .catch(next)
-// })
-
 // CREATE
-// POST /guests
-router.post('/guests', requireToken, (req, res, next) => {
+router.post('/guests/:waitlistId', requireToken, (req, res, next) => {
   req.body.guest.owner = req.user.id
   const data = req.body.guest
-  const waitlistId = data.waitlistId
+  const waitlistId = req.params.waitlistId
   Waitlist.findById(waitlistId)
     .then(handle404)
     .then(waitlist => {
@@ -50,12 +29,11 @@ router.post('/guests', requireToken, (req, res, next) => {
 })
 
 // UPDATE
-// PATCH /guests/5a7db6c74d55bc51bdf39793
-router.patch('/guests/:id', (req, res, next) => {
+router.patch('/guests/:waitlistId/:guestId', (req, res, next) => {
   // delete req.body.guest.owner
-  const guestId = req.params.id
+  const guestId = req.params.guestId
+  const waitlistId = req.params.waitlistId
   const data = req.body.guest
-  const waitlistId = data.waitlistId
   Waitlist.findById(waitlistId)
     .then(handle404)
     .then(waitlist => {
@@ -68,7 +46,6 @@ router.patch('/guests/:id', (req, res, next) => {
 })
 
 // DESTROY
-// DELETE /guests/5a7db6c74d55bc51bdf39793
 router.delete('/guests/:waitlistId/:guestId', (req, res, next) => {
   const guestId = req.params.guestId
   const waitlistId = req.params.waitlistId
